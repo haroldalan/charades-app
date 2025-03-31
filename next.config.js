670bb5ffc648e6760cf9f6f2580fa67935fc4b88
haroldalan <tharoldalan@gmail.com>
2025-04-01 05:08:17 +0530

    Update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-  };
-  
-  export default nextConfig;
-  
+  // Your Next.js configuration
+};
+
+module.exports = nextConfig;
